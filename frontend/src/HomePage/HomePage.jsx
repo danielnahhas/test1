1acf32533cffd3d2ff0945c228a4e1377b2f660b
Daniel Nahhas <danielnahhas1999@gmail.com>
2023-10-10 21:00:14 +0300

    change

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ function HomePage() {
     <div>
       <img src="/assets/Logo.jpg" alt="pic" />
       <p>Let's build your ideal gaming pc </p>
+      <p>Let's build your ideal gaming pc 2 </p>
     </div>
   );
 }
